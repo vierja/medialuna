d1be3dd300eebd09bb77add8b509a4c79deb5407
Javier Rey <javirey@gmail.com>
2013-06-03 18:00:00 -0300

    Primera version del analisis lexico usando flex

---
+++ test/test1.lua
@@ -0,0 +1,20 @@
+function test_function(name)
+    -- body
+    print("Mi nombre es "..name)
+end
+
+test_function(name)
+
+--[[
+    Mi nombre es Javier
+
+]]
+
+function hello( what, is, this )
+    what = [[
+        Sin linea arriba""'''
+    ]]
+    is = [[  
+        Con linea arriba""''
+    ]]
+end
